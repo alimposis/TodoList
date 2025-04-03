6e57b307b77ebd62c47a1d79afaa9b60c9c2ab29
Alimpos <alimpostea@mail.ru>
2025-04-04 02:18:38 +0300

    Новый коммит

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,4 +1,4 @@
-import { Box, Checkbox, CloseButton, Code, Flex, Stack, Text } from "@chakra-ui/react";
+import { Checkbox, CloseButton, Stack, Text } from "@chakra-ui/react";
 
 import { ITodo } from "@/models";
 
